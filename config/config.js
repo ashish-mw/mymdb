ff02eb7a88ee6be98f7da3d0f0aef31dab1fe509
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2023-01-12 09:47:19 +0530

    clean up

--- config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  username: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "postgres",
-  database: process.env.DB_DATABASE || "mymdb",
-  host: process.env.DB_HOST || "127.0.0.1",
-  dialect: process.env.DB_DIALECT || "postgres",
-};
-
-module.exports = config;
